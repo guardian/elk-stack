b91d2f0bfe79b428247f1d7a0cb0e2872bf810d2
Nick Satterly <nick.satterly@guardian.co.uk>
2014-07-10 21:10:44 +0100

    add logcabin config to cfn templates

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    'host': '@@LOGCABIN_HOST',
+    'listen_port': 9201,
+    'cookie_secret': ';5AgY<SX1sk0bA]AVfiOr3add2U]ui`LK6[3/AG:fac>Fn=WDh/rma0DbOR6IxPI:',
+    'oauth_unauthenticated': ['/__es/'],
+    // 'oauth_application_name': 'logcabin',
+    'oauth_client_id': '@@CLIENT_ID',
+    'oauth_client_secret': '@@CLIENT_SECRET',
+    'es_host': 'localhost',
+    'es_port': 9200
+}
