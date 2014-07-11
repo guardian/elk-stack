231d05de8a2a564618ecae0c7d3adf8f790a028c
Nick Satterly <nick.satterly@guardian.co.uk>
2014-07-11 12:21:49 +0100

    more minor fixes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports =  {
     'host': '@@LOGCABIN_HOST',
-    'listen_port': 9201,
+    'listen_port': 8080,
     'cookie_secret': ';5AgY<SX1sk0bA]AVfiOr3add2U]ui`LK6[3/AG:fac>Fn=WDh/rma0DbOR6IxPI:',
     'oauth_unauthenticated': ['/__es/'],
     // 'oauth_application_name': 'logcabin',
