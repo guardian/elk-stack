06d2fc7807c6e2260c04f49f873d7a700b702ec6
Simon Hildrew <simon@hildrew.net>
2015-02-10 11:59:06 +0000

    Add build script

---
+++ packer/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Build packer AMI (on TeamCity host)
+
+# die if any command fails
+set -e
+
+# set PACKER_HOME if it isn't already provided
+[ -z "${PACKER_HOME}" ] && PACKER_HOME=/opt/packer
+
+# ensure that we have AWS credentials (configure in TeamCity normally)
+if [ -z "${AWS_ACCESS_KEY}" -o -z "${AWS_SECRET_KEY}" ]
+then
+  echo "AWS_ACCESS_KEY and AWS_SECRET_KEY environment variables must be set" > &2
+  exit 1
+fi
+
+# now build
+echo "Building ELK AMI"
+${PACKER_HOME}/packer build -color=false elk.json
