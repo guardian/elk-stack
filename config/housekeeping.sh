3457f69d27ff8ecd48c0a588836e23818fa05a8d
Simon Hildrew <simon.hildrew@theguardian.com>
2015-03-11 14:11:23 +0000

    Fix american spelling

--- config/housekeeping.sh
+++ config/housekeeping.sh
@@ -3,4 +3,4 @@
 /usr/local/bin/curator --logformat logstash delete --older-than 7 | /bin/nc localhost 28778
 # Optimise older indexes
 /usr/local/bin/curator --logformat logstash bloom --older-than 1 | /bin/nc localhost 28778
-/usr/local/bin/curator --logformat logstash optimise --older-than 1 | /bin/nc localhost 28778
+/usr/local/bin/curator --logformat logstash optimize --older-than 1 | /bin/nc localhost 28778
